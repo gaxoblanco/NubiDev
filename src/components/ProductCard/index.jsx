41ffadd252bdab8b60762bfa3be3730e9487e554
gaxoblanco <94192138+gaxoblanco@users.noreply.github.com>
2022-08-25 17:12:45 -0300

    [Eligue cuantas unidades]
    Paso intermedio para agregar un producto al carrito

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -4,48 +4,14 @@ import {Btn} from '../css/Button';
 import {Price, Parrafo, StateStock, IconStar, OnOff} from '../css/styles';
 
 
-const initialState = {
-    favorites: []
-  }
 
-  const favoriteReducer = (state, action) =>{
-    switch(action.type){
-        case 'ADD_TO_FAVORITE':
-            return{
-                ...state, favorites: [...state.favorites, action.payload]
-            };
-        default:
-            return state;
-    };
-  }
+export const ProductCard = ({product, unitsProduct, setUnits, units})=>{
 
-
-
-export const ProductCard = ({product, handleAddToCart})=>{
-
-    // const [products, setProducts] = useState([]);
-    const [favorites, dispatch] = useReducer(favoriteReducer, initialState)
-
-
-    useEffect(()=>{
-        // fetch( lista )
-        // .then( response => response.json())
-        // .then( data => setStock(data.results));
-
-    }, [])
-
-    
-    let isFavorite = false;
-    const favoriteClick = favorite =>{
-        // dispatch({type: 'ADD_TO_FAVORITE', payload: favorite});
-        console.log(isFavorite);
-        isFavorite = !isFavorite;
-        return isFavorite;
-    }
 
 
     return (
         <div className="bg-red-400 w-11/12 flex relative">
+        
 
             <section className="bg-red-400 w-11/12 flex relative">
                 <OnOff 
@@ -65,7 +31,7 @@ export const ProductCard = ({product, handleAddToCart})=>{
                             <Btn
                                 type="button" 
                                 isGreen={true}
-                                onClick={handleAddToCart(product)}
+                                onClick={()=> {setUnits(!units), unitsProduct(product)}}
                             >
                                 Agregar
                             </Btn>
@@ -73,7 +39,7 @@ export const ProductCard = ({product, handleAddToCart})=>{
                     </div>
                 </div>
                 <a href="#" >
-                <IconStar isOn={isFavorite} fill="currentColor" className="bi bi-star-fill" viewBox="0 0 16 16">
+                <IconStar isOn={false} fill="currentColor" className="bi bi-star-fill" viewBox="0 0 16 16">
                 <path d="M3.612 15.443c-.386.198-.824-.149-.746-.592l.83-4.73L.173 6.765c-.329-.314-.158-.888.283-.95l4.898-.696L7.538.792c.197-.39.73-.39.927 0l2.184 4.327 4.898.696c.441.062.612.636.282.95l-3.522 3.356.83 4.73c.078.443-.36.79-.746.592L8 13.187l-4.389 2.256z"/>
                 </IconStar>
                 </a>
--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,25 +1,52 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import {ProductCard} from '../ProductCard';
+import {AddCart} from '../AddCart';
 import {AppContext} from '../../context/AppContex';
 
+import './style.css';
+
 export const ProductList = () => {
+  const [units, setUnits] = useState(false);
   const {state, addToCart} = useContext(AppContext);
   const {products} = state;
 
+  const [itemData, setItemData] = useState({})
+
+
   const handleAddToCart = product => () =>{
     addToCart(product);
+    setUnits(false) 
   }
+
+  const unitsProduct = (ItemUnits) =>{
+    setItemData (ItemUnits);
+  }
+
+  
   return (
-    <div>
+      <div>
+
+        <div className="absolute z-10 bg-slate-400">
+          {units ? 
+            <AddCart 
+              handleAddToCart = {handleAddToCart}
+              itemData = {itemData}
+              setUnits = {setUnits}
+            /> : null}
+        </div>
+        
         <div>
             {products.map(product =>(
                 <ProductCard 
                   key={product._id} 
                   product={product} 
-                  handleAddToCart={handleAddToCart}
-                  />
+                  units={units}
+                  setUnits = {setUnits}
+                  unitsProduct = {unitsProduct}
+                />
             ))}
         </div>
+
     </div>
   )
 }
