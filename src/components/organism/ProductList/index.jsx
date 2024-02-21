f2c90854d06a69d1dc2bff4d96a975bd9c8b4753
Gaston <gaston@tdil.com.ar>
2024-02-21 11:35:33 -0300

    style filter input

--- src/components/organism/ProductList/index.jsx
+++ src/components/organism/ProductList/index.jsx
@@ -58,8 +58,12 @@ export const ProductList = () => {
       <section className="shadow-component w-72 peach-fuzz--300 bg-[#FBD5CA] rounded-xl max-h-[600px] p-5">
         <h2 className="mb-4">Filtrar productos</h2>
         <form className="search-form mb-4 relative text-base">
-          <input type="text" placeholder="Buscar productos" />
-          <button type="submit" className="w-6 absolute">
+          <input
+            type="text"
+            placeholder="Buscar productos"
+            className="p-1 mr-[-2rem] rounded-md"
+          />
+          <button type="submit" className="w-8 p-1.5 absolute">
             <svg fill="currentColor" viewBox="0 0 16 16">
               <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
             </svg>
